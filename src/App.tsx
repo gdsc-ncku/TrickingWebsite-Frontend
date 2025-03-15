cba0ed6d51272c018dd56ce9f1083c59ece810a6
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2025-03-15 23:59:26 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { Routes } from "react-router-dom";
+
+export default function App(): ReactNode {
+    return <>
+        <Routes>
+
+        </Routes>
+    </>;
+};
