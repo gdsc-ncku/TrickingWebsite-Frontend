75dfebadbc980c17f15da081e6dc216d5b8aa587
mario3443 <holyangel12yt@gmail.com>
2025-05-22 00:07:35 +0800

    新增 Trickdetail 元件，完成Trick介紹頁面初版

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from "react";
-import { Route, Routes } from 'react-router-dom';
-import MainPage from "./pages/MainPage.tsx"
+import { Component, ReactNode } from "react";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage.tsx";
 
 export default function App(): ReactNode {
-    return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-        </Routes>
-    );
-};
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
+}
